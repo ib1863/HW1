87df1fbb2136118beef0830284e029f764705f3a
test <ib1863@bs.amalnet.k12.il>
2022-01-05 15:08:13 +0200

    123

---
+++ app/src/main/java/com/example/hw1/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.hw1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btnChange);
+    }
+
+    public void clicking(View view) {
+       btn.setText("Oh, yea, Ive been clicked");
+    }
+}
